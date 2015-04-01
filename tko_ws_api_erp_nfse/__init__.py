59dff6305607e9c57e3c60502483f4aa52b3c9b6
cmsalmeida <carlos.almeida@tkobr.com>
2015-04-01 12:46:19 -0300

    create module tko_ws_api_erp_nfse

---
+++ tko_ws_api_erp_nfse/__init__.py
@@ -0,0 +1,11 @@
+# -*- encoding: utf-8 -*-
+##############################################################################
+#
+#    Thinkopen Brasil
+#    Copyright (C) Thinkopen Solutions Brasil (<http://www.tkobr.com>).
+#
+#    This program is NOT free software.
+#
+##############################################################################
+
+import nfse
