49684370c60b75187f0c2a29c3c3a0de990105f6
cmsalmeida <carlos.almeida@tkobr.com>
2015-04-02 07:15:42 -0300

    add tko_project_task_type

---
+++ tko_project_task_type/project_task.py
@@ -0,0 +1,68 @@
+# -*- encoding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    Copyright (C) 2004-2010 Tiny SPRL (<http://tiny.be>).
+#
+#    Thinkopen Brasil
+#    Copyright (C) Thinkopen Solutions Brasil (<http://www.tkobr.com>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+from openerp import models, api, fields, _
+
+
+class task_type(models.Model):
+    _name = 'task.type'
+    
+    name = fields.Char(string='Name', required=True)
+    color = fields.Integer('Color Index', size=1)
+    task_id = fields.Many2one('project.task', string='Task')
+    
+class project_task(models.Model):
+    _inherit = 'project.task'
+    
+    type_name = fields.Char(compute='_get_type_name', store=True, string='Name')
+    task_type_id = fields.Many2one('task.type', string='Type')
+    color = fields.Integer(compute='_get_color', string='Color', store=False)
+    
+    @api.multi
+    def name_get(self):
+        result = []
+        for task in self:
+            task_type = task.task_type_id and task.task_type_id.name or ''
+            result.append((task.id, "%s %s" % ('[' + str(task_type) + ']', task.name or ' ')))
+        return result
+            
+    
+    @api.depends('task_type_id.name')
+    def _get_type_name(self):
+        for record in self:
+            if record.task_type_id:
+                record.type_name = record.task_type_id.name
+                
+    @api.depends('task_type_id.color')
+    def _get_color(self):
+        for record in self:
+            
+            if record.task_type_id:
+                record.color = record.task_type_id.color
+    
+    @api.onchange('task_type_id')
+    def _change_task_type(self):
+        if self.task_type_id:
+            self.color = str(self.task_type_id.color)[-1]
+            self.type_name = self.task_type_id.name
