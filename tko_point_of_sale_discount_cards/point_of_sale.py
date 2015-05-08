06124512ad2ccef1e0ce0917743e901acbed5589
cmsalmeida <carlos.almeida@tkobr.com>
2015-05-08 02:02:20 -0300

    add css formating and format code

--- tko_point_of_sale_discount_cards/point_of_sale.py
+++ tko_point_of_sale_discount_cards/point_of_sale.py
@@ -6,18 +6,16 @@ class pos_discount_cards(models.Model):
     name = fields.Char('Name', required=True)
     type = fields.Selection([('p', 'Percentage'), ('fi', 'Fixed')], string='Type', required=True)
     value = fields.Float('Value', required=True)
-    active = fields.Boolean('Active', default = True)
-    
+    active = fields.Boolean('Active', default=True)
 
 
 class pos_config(models.Model):
     _inherit = 'pos.config'
     
+    discount_card_ids = fields.Many2many('pos.discount.cards', string='Discount cards')
     
-    discount_card_ids = fields.Many2many('pos.discount.cards', string= 'Discount cards')
     
 class pos_session(models.Model):
     _inherit = 'pos.session'
     
-    
-    discount_card_ids = fields.Many2many('pos.discount.cards', 'pos_session_cards_rel', 'session_id','card_id', related='config_id.discount_card_ids', readonly=True, string='Available Discount Cards')
+    discount_card_ids = fields.Many2many('pos.discount.cards', 'pos_session_cards_rel', 'session_id', 'card_id', related='config_id.discount_card_ids', readonly=True, string='Available Discount Cards')
--- tko_point_of_sale_discount_cards/static/src/js/widgets.js
+++ tko_point_of_sale_discount_cards/static/src/js/widgets.js
@@ -1,30 +1,29 @@
 
 function pos_discount_cards_widgets(instance, module){ //module is instance.point_of_sale
-        var QWeb = instance.web.qweb;
-	var _t = instance.web._t;
-	
-	
-	/*self.$('#discount-card-select').change(function(){
+    var QWeb = instance.web.qweb;
+    var _t = instance.web._t;
+    
+    
+    /*self.$('#discount-card-select').change(function(){
         var name = this.value;
         self.discount_card_change(name);
     })*/
-		
 
-	module.PaymentScreenWidget = module.PaymentScreenWidget.extend({
-		
-		events: {
-	        "change .discount-card-select": "selectCard" 
-	    },
+    module.PaymentScreenWidget = module.PaymentScreenWidget.extend({
+        
+        events: {
+            "change .discount-card-select": "selectCard" 
+        },
 
-	    selectCard: function(e){
-	    	this.pos_widget.order_widget.update_summary();
-	    	this.pos_widget.payment_screen.update_payment_summary();
-	        console.log("....selectCard....",this.$('.discount-card-select').val());
-		    },
-		    
-		    
-		update_payment_summary: function() {
-			this._super();
+        selectCard: function(e){
+            this.pos_widget.order_widget.update_summary();
+            this.pos_widget.payment_screen.update_payment_summary();
+            console.log("....selectCard....",this.$('.discount-card-select').val());
+            },
+            
+            
+        update_payment_summary: function() {
+            this._super();
             var currentOrder = this.pos.get('selectedOrder');
             var selected = $('#discount-card-select option:selected').attr('value');
             var paidTotal = currentOrder.getPaidTotal();
@@ -32,9 +31,9 @@ function pos_discount_cards_widgets(instance, module){ //module is instance.poin
             var remaining = dueTotal > paidTotal ? dueTotal - paidTotal : 0;
             var change = paidTotal > dueTotal ? paidTotal - dueTotal : 0;
             if (dueTotal< 0){
-            	dueTotal = 0;
-            	remaining = 0;
-            	change = 0;
+                dueTotal = 0;
+                remaining = 0;
+                change = 0;
             }
             this.$('.payment-due-total').html(this.format_currency(dueTotal));
             this.$('.payment-remaining').html(this.format_currency(remaining));
@@ -48,37 +47,27 @@ function pos_discount_cards_widgets(instance, module){ //module is instance.poin
                 this.pos_widget.action_bar.set_button_disabled('invoice', !this.is_paid());
             }
         },
-		
-		
-	    });
+    });
 
-    
-    
-    // compute discount of 
-    
-   
+
+// compute discount of
 var OrderSuper = module.Order;
 module.Order = module.Order.extend({
-	
-	
-	
-	//send discount_card_id to write in database
-	export_as_JSON: function() {
-		var res = OrderSuper.prototype.export_as_JSON.call(this);
-		var discount_card_id = self.$('.discount-card-select option:selected').attr('id');
-		res.discount_card_id = discount_card_id || false;
-	    
-	    return res;
-	},
-	
-	
-	//this method returns discount given by discount card
-	getDiscountCard : function(){
-		var discount_id = self.$('.discount-card-select option:selected').attr('id');
-		var discount_type = self.$('.discount-card-select option:selected').attr('type');
-		var discount_value = self.$('.discount-card-select option:selected').attr('value');
-
-		var subtotal = (this.get('orderLines')).reduce((function(sum, orderLine) {
+    //send discount_card_id to write in database
+    export_as_JSON: function() {
+        var res = OrderSuper.prototype.export_as_JSON.call(this);
+        var discount_card_id = self.$('.discount-card-select option:selected').attr('id');
+        res.discount_card_id = discount_card_id || false;
+        
+        return res;
+    },
+    
+    //this method returns discount given by discount card
+    getDiscountCard : function(){
+        var discount_id = self.$('.discount-card-select option:selected').attr('id');
+        var discount_type = self.$('.discount-card-select option:selected').attr('type');
+        var discount_value = self.$('.discount-card-select option:selected').attr('value');
+        var subtotal = (this.get('orderLines')).reduce((function(sum, orderLine) {
             return sum + orderLine.get_price_with_tax();
         }), 0);
         var discount = 0.0;
@@ -95,20 +84,13 @@ module.Order = module.Order.extend({
                     }
             }
         return discount;
-
-
-		
-	},
-	
-	
-	
+    },
 
     getTotalTaxIncluded: function() {
-          
-    	var discount_id = self.$('.discount-card-select option:selected').attr('id');
-		var discount_type = self.$('.discount-card-select option:selected').attr('type');
-		var discount_value = self.$('.discount-card-select option:selected').attr('value');
-		
+        var discount_id = self.$('.discount-card-select option:selected').attr('id');
+        var discount_type = self.$('.discount-card-select option:selected').attr('type');
+        var discount_value = self.$('.discount-card-select option:selected').attr('value');
+        
         var subtotal = (this.get('orderLines')).reduce((function(sum, orderLine) {
             return sum + orderLine.get_price_with_tax();
         }), 0);
@@ -128,42 +110,25 @@ module.Order = module.Order.extend({
             }
         total = subtotal - discount;
         return total;
-            
         }
 });
 
 
-
-
-
 module.OrderWidget = module.OrderWidget.extend({
-	
-	update_summary: function(){
-		this._super();
-		
-		var order = this.pos.get('selectedOrder');
-		var total     = order ? order.getTotalTaxIncluded() : 0;
-		var discount     = order ? order.getDiscountCard() : 0;
-		var taxes     = order ? order.getTotalTaxExcluded(): 0;
-		final_tax = Number(total) + Number(discount) - Number(taxes) 
-		if (total < 0){
-			total = 0;
-		}
-		this.el.querySelector('.summary .total > .value').textContent = this.format_currency(total);
-	    this.el.querySelector('.summary .total .subentry .value').textContent = this.format_currency(final_tax );
-	    this.el.querySelector('.summary .total .discount .value').textContent = this.format_currency(-discount);
-	    
-	    
+    update_summary: function(){
+        this._super();
+        
+        var order = this.pos.get('selectedOrder');
+        var total     = order ? order.getTotalTaxIncluded() : 0;
+        var discount     = order ? order.getDiscountCard() : 0;
+        var taxes     = order ? order.getTotalTaxExcluded(): 0;
+        final_tax = Number(total) + Number(discount) - Number(taxes) 
+        if (total < 0){
+            total = 0;
+        }
+        this.el.querySelector('.summary .total > .value').textContent = this.format_currency(total);
+        this.el.querySelector('.summary .total .subentry .value').textContent = this.format_currency(final_tax );
+        this.el.querySelector('.summary .total .discount .value').textContent = this.format_currency(-discount);
     },
-	});
-
-
-
-
-
-
-
-
-
-
+    });
 } //end of code
