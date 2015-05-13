0186348dbec75f2e545f2b42487afac5e7ae55d2
cmsalmeida <carlos.almeida@tkobr.com>
2015-05-13 19:06:08 -0300

    add discount_card_id in order and store discount card in database

--- tko_point_of_sale_discount_cards/point_of_sale.py
+++ tko_point_of_sale_discount_cards/point_of_sale.py
@@ -1,5 +1,8 @@
 from openerp import fields, models, api, _
 
+import logging
+_logger = logging.getLogger(__name__)
+
 class pos_discount_cards(models.Model):
     _name = 'pos.discount.cards'
     
@@ -19,3 +22,32 @@ class pos_session(models.Model):
     _inherit = 'pos.session'
     
     discount_card_ids = fields.Many2many('pos.discount.cards', 'pos_session_cards_rel', 'session_id', 'card_id', related='config_id.discount_card_ids', readonly=True, string='Available Discount Cards')
+
+
+class pos_order(models.Model):
+    _inherit = 'pos.order'
+    
+    discount_card_id = fields.Many2one('pos.discount.cards' , string='Discount Cards')
+    
+    def create_from_ui(self, cr, uid, orders, context=None):
+        # Keep only new orders
+        
+        
+        pos_obj = self.pool.get('pos.order')
+        pos_line_object = self.pool.get('pos.order.line')
+        table_reserved_obj = self.pool.get("table.reserverd")
+        session_obj = self.pool.get('pos.session')
+        shop_obj = self.pool.get('sale.shop')
+        order_ids = super(pos_order,self).create_from_ui(cr, uid, orders, context = context)
+        #write discount_card_id to order
+        if len(order_ids) == len(orders):
+            i = 0
+            for tmp_order in orders:
+                
+                if 'data' in tmp_order.keys():
+                    discount_card_id = tmp_order['data'].get('discount_card_id')
+                    if discount_card_id:
+                        pos_obj.write(cr, uid, [order_ids[i]],{'discount_card_id' : discount_card_id})
+                    i = i + 1
+        return order_ids
+        
