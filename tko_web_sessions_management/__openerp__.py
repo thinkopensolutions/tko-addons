a3c7fe1ece554ac1c43ce88fada9d383f3249ad4
cmsalmeida <carlos.almeida@tkobr.com>
2015-09-12 23:15:25 -0300

    tko_web_sessions_management: update help messages

--- tko_web_sessions_management/__openerp__.py
+++ tko_web_sessions_management/__openerp__.py
@@ -24,7 +24,7 @@
 
 {
     'name': 'TKOBR - Odoo Web Sessions Management Rules',
-    'version': '1.034',
+    'version': '1.035',
     'category': 'Tools',
     'sequence': 15,
     'summary': 'Manage Users Login Rules in Odoo.',
