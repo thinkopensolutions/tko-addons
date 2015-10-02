a9f17cf176545981906c64e06a2f3d4f6cf45b37
cmsalmeida <carlos.almeida@tkobr.com>
2015-10-02 10:16:09 -0300

    tko_account_contract_report_template: change report model to account.analytic.account from contract.template.wizard

--- tko_account_contract_report_template/__openerp__.py
+++ tko_account_contract_report_template/__openerp__.py
@@ -24,7 +24,7 @@
 
 {
     'name': 'Account Contract Report Template',
-    'version': '0.050',
+    'version': '0.051',
     'category': 'Account',
     'sequence': 38,
     'complexity': 'normal',
