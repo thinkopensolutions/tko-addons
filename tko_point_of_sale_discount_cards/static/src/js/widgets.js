bddf6e8da31e3d6f1765232e4ed240647f7870a3
cmsalmeida <carlos.almeida@tkobr.com>
2016-01-04 20:54:09 -0200

    correct order total computation

--- tko_point_of_sale_discount_cards/static/src/js/widgets.js
+++ tko_point_of_sale_discount_cards/static/src/js/widgets.js
@@ -62,7 +62,7 @@ module.Order = module.Order.extend({
         var discount_type = self.$('.discount-card-select option:selected').attr('type');
         var discount_value = self.$('.discount-card-select option:selected').attr('value');
         var subtotal = (this.get('orderLines')).reduce((function(sum, orderLine) {
-            return sum + orderLine.get_price_without_tax(); //display price is computed with considering both discount types fixed and precentage 
+            return sum + orderLine.get_display_price(); //display price is computed with considering both discount types fixed and precentage 
         }), 0);
         var discount = 0.0;
         var total = 0.0
@@ -82,7 +82,7 @@ module.Order = module.Order.extend({
 
     getTotalTaxIncluded: function() {
         var subtotal = (this.get('orderLines')).reduce((function(sum, orderLine) {
-            return sum + orderLine.get_price_without_tax(); //display price is computed with considering both discount types fixed and precentage 
+            return sum + orderLine.get_display_price(); //display price is computed with considering both discount types fixed and precentage 
         }), 0);
         
         var discount = 0.0;
