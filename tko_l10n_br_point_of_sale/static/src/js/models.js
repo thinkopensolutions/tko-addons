bc40fd785413e464e3c457143cd64a222840bb6d
cmsalmeida <carlos.almeida@tkobr.com>
2016-05-05 00:05:29 -0300

    load fiscal classification and tax.definition.sale in pos

--- tko_l10n_br_point_of_sale/static/src/js/models.js
+++ tko_l10n_br_point_of_sale/static/src/js/models.js
@@ -14,6 +14,24 @@ function l10n_br_fields(instance, module){ //module is instance.point_of_sale
 	  return m.fields.push('average_federal_tax','average_state_tax'), 
 	  m;
 	   });
+	
+	module.PosModel.prototype.models.filter(function (m)
+			 { return m.model === 'product.product'; }).map(function (m) {
+			  return m.fields.push('property_fiscal_classification'), 
+			  m;
+			   });
+	
+	// tax definition with tax codes
+	module.PosModel.prototype.models.push({
+            model:  'l10n_br_tax.definition.sale',
+            fields: ['tax_id','tax_code_id','fiscal_classification_id'],
+           
+            loaded: function(self, tax_definitions){
+                self.tax_definitions = tax_definitions;
+            },
+        
+	});
+
 	//module.PosModel.prototype.models[5].fields.push('street2','cnpj_cpf');
 	
 	module.PosDB.include({
