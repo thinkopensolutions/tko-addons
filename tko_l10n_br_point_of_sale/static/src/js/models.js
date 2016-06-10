f963c217154b764fcdee94277a8442c94057b885
cmsalmeida <carlos.almeida@tkobr.com>
2016-06-10 17:46:40 -0300

    pass order_reference in pos, keep order sequence as default value

--- tko_l10n_br_point_of_sale/static/src/js/models.js
+++ tko_l10n_br_point_of_sale/static/src/js/models.js
@@ -11,7 +11,7 @@ function l10n_br_fields(instance, module){ //module is instance.point_of_sale
 	   
 	module.PosModel.prototype.models.filter(function (m)
 	 { return m.model === 'res.company'; }).map(function (m) {
-	  return m.fields.push('average_federal_tax','average_state_tax'), 
+	  return m.fields.push('average_federal_tax','average_state_tax','order_reference'), 
 	  m;
 	   });
 	//module.PosModel.prototype.models[5].fields.push('street2','cnpj_cpf');
