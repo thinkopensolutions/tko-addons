1d7301a2009c218759febe6d830ba2c1e61229ab
cmsalmeida <carlos.almeida@tkobr.com>
2016-09-18 12:49:44 +0100

    Fix all message sent HEADERS

--- tko_mail_smtp_per_user/__openerp__.py
+++ tko_mail_smtp_per_user/__openerp__.py
@@ -29,7 +29,7 @@
     'author': 'ThinkOpen Solutions Brasil',
     'license': 'AGPL-3',
     'website': 'http://tkobr.com',
-    'version': '0.006',
+    'version': '1.0',
     'sequence': 10,
     'depends': [
         'base',
