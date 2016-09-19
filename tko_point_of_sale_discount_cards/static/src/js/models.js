ee74db3a467c2bd5a17bd2becd2358cfbe59f82c
Yogesh Kushwaha <yogeshkushwaha4@gmail.com>
2016-09-19 12:09:01 -0300

    show only filtered discount cards based on pos config

--- tko_point_of_sale_discount_cards/static/src/js/models.js
+++ tko_point_of_sale_discount_cards/static/src/js/models.js
@@ -4,16 +4,27 @@ function pos_discount_cards_model(instance, module){ //module is instance.point_
 
     module.PosModel.prototype.models.filter(function (m)
      { return m.model === 'pos.session'; }).map(function (m) {
-      return m.fields.push('discount_card_ids'), 
+      return m.fields.push('discount_card_ids'),
       m;
        });
 
+    // set allowed discount cards in pos
     module.PosModel.prototype.models.push(
             {
                 model:  'pos.discount.cards',
                 fields: ['name', 'type', 'value' , 'active' ],
-                loaded: function(self,cards){ 
-                    self.cards = cards;
+                loaded: function(self,cards){
+                    filterd_cards = []
+                    // filter cards allowed in configuration
+                    allowed_cards = self.pos_session.discount_card_ids;
+                    _.filter(cards, function(card){
+                        if (allowed_cards.indexOf(card.id)!== -1)
+                        {
+                            filterd_cards.push(card);
+                        }
+
+                    })
+                    self.cards = filterd_cards;
                 }
             }
     )
