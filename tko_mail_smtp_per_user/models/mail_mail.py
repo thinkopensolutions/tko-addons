133bad96ad5eb95eede59ad807692c6c2b2ab448
zimbora69 <alexandre.ruffer@tkobr.com>
2017-01-23 02:36:32 -0200

    pep8 fix

--- tko_mail_smtp_per_user/models/mail_mail.py
+++ tko_mail_smtp_per_user/models/mail_mail.py
@@ -1,32 +1,15 @@
-# -*- encoding: utf-8 -*-
-##############################################################################
-#
+# -*- coding: utf-8 -*-
 #    OpenERP, Open Source Management Solution
 #    Copyright (C) 2004-2010 Tiny SPRL (<http://tiny.be>).
-#
-#    ThinkOpen Solutions Brasil
-#    Copyright (C) Thinkopen Solutions <http://www.tkobr.com>.
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
+#    Thinkopen - Brasil
+#    Copyright (C) Thinkopen Solutions (<http://www.thinkopensolutions.com.br>)
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
 from odoo import models, fields
 from odoo.addons.base.ir.ir_mail_server import extract_rfc2822_addresses
 
 
-class ir_mail_server(models.Model):
+class IrMailServer(models.Model):
     _inherit = 'ir.mail_server'
 
     user_id = fields.Many2one('res.users', string='Owner')
@@ -41,11 +24,11 @@ class ir_mail_server(models.Model):
         server_id = self.env['ir.mail_server'].search([('smtp_user', '=', from_rfc2822)])
         if server_id and server_id[0]:
             message['Return-Path'] = from_rfc2822
-        return super(ir_mail_server, self).send_email(message, mail_server_id, smtp_server, smtp_port,
-                                                      smtp_user, smtp_password, smtp_encryption, smtp_debug)
+        return super(IrMailServer, self).send_email(message, mail_server_id, smtp_server, smtp_port,
+                                                    smtp_user, smtp_password, smtp_encryption, smtp_debug)
 
 
-class mail_mail(models.Model):
+class MailMail(models.Model):
     _inherit = 'mail.mail'
 
     def send(self, auto_commit=False, raise_exception=False):
@@ -55,4 +38,4 @@ class mail_mail(models.Model):
             server_id = server_id and server_id[0] or False
             if server_id:
                 self.write({'mail_server_id': server_id[0].id, 'reply_to': email.email_from})
-        return super(mail_mail, self).send(auto_commit=auto_commit, raise_exception=raise_exception)
+        return super(MailMail, self).send(auto_commit=auto_commit, raise_exception=raise_exception)
