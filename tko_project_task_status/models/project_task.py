f727cc186c10c5f32acfd18787b3083c044312e9
Yogesh Kushwaha <yogeshkushwaha4@gmail.com>
2017-02-16 20:42:20 -0200

    create module tko_project_task_status

---
+++ tko_project_task_status/models/project_task.py
@@ -0,0 +1,45 @@
+# -*- encoding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    Copyright (C) 2004-2010 Tiny SPRL (<http://tiny.be>).
+#
+#    ThinkOpen Solutions Brasil
+#    Copyright (C) Thinkopen Solutions <http://www.tkobr.com>.
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+from odoo import models, api, fields
+
+
+class ProjectTaskStatus(models.Model):
+    _name = 'project.task.status'
+
+    name = fields.Char(string='Name', required=True)
+    expected_duration = fields.Integer(u'Expected Time')
+    expected_duration_unit= fields.Selection([('d','Day'),('w','Week'),('m','Month'),('y','Year')],u'Expected Time Unit')
+    task_id = fields.Many2one('project.task','Task')
+
+class ProjectTask(models.Model):
+    _inherit = 'project.task'
+
+    status_ids = fields.One2many('project.task.status','task_id','Status')
+
+
+
+
+
+
