eab544344543707d345ca597c6724d3f1e5637d7
Chauhan Chirag <chiragsinhchauhan30@gmail.com>
2017-04-20 20:34:31 +0530

    Add a field nickname on partner.

---
+++ tko_base_partner_nickname/models/res_partner.py
@@ -0,0 +1,47 @@
+# -*- encoding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    Copyright (C) 2004-2010 Tiny SPRL (<http://tiny.be>).
+#
+#    ThinkOpen Solutions Brasil
+#    Copyright (C) Thinkopen Solutions <http://www.tkobr.com>.
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+from openerp import models, fields, api
+
+class ResPartner(models.Model):
+    _inherit = 'res.partner'
+
+    nickname = fields.Char('Nickname')
+
+    @api.multi
+    def name_get(self):
+        result = []
+        for record in self:
+            name = record.nickname and "[" + record.nickname +"] " + record.name or record.name
+            result.append((record.id, name))
+        return result
+
+    @api.model
+    def name_search(self, name, args=None, operator='ilike', limit=100):
+        if not args:
+            args.insert(0, ['name', operator, name])
+        args.insert(0, ['nickname', operator, name])
+        args.insert( 0, '|')
+        res = super(ResPartner, self).name_search(name, args=args, operator=operator, limit=limit)
+        return res
