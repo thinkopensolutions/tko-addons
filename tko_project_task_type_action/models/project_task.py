ee70d6c32ac8e79c9442e4b1809d7aa0abb27b10
Carlos Aguda <carlos.aguda@tkobr.com>
2017-05-06 18:34:49 -0300

    fixed views and added task type in action line

--- tko_project_task_type_action/models/project_task.py
+++ tko_project_task_type_action/models/project_task.py
@@ -22,14 +22,14 @@
 #
 ##############################################################################
 
-from odoo import models, api, fields
+from odoo import models, fields
 
 
 class task_type(models.Model):
     _inherit = 'task.type'
 
     action_line_ids = fields.Many2many('project.task.action', 'project_task_type_action_rel', 'type_id', 'action_id',
-                                       'Action')
+                                       'Actions')
 
 
 class ProjectTask(models.Model):
@@ -37,3 +37,10 @@ class ProjectTask(models.Model):
 
     related_action_line_ids = fields.Many2many('project.task.action', 'project_task_action_rel', 'task_id', 'action_id',
                                                related='task_type_id.action_line_ids', string='Action')
+
+
+class ProjectTaskActionsLine(models.Model):
+    _inherit = 'project.task.action.line'
+
+
+    task_type_id = fields.Many2one(u'Task Type', related='task_id.task_type_id', readonly=True)
