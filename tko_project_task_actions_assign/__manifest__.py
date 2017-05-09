bf224984ff59dbf4e3d4d3886de60086ab5aec2f
Yogesh Kushwaha <yogeshkushwaha4@gmail.com>
2017-05-09 14:19:36 -0300

    rebase 10.0

--- tko_project_task_actions_assign/__manifest__.py
+++ tko_project_task_actions_assign/__manifest__.py
@@ -12,20 +12,21 @@
     'version': '10.0.0.0.0',
     'application': False,
     'installable': True,
+
     'auto_install': False,
     'depends': [
-                'tko_project_task_actions',
+        'tko_project_task_actions',
     ],
     'external_dependencies': {
-                                'python': [],
-                                'bin': [],
-                                },
+        'python': [],
+        'bin': [],
+    },
     'init_xml': [],
     'update_xml': [],
     'css': [],
     'demo_xml': [],
     'test': [],
     'data': [
-             'views/project_task_view.xml',
+        'views/project_task_view.xml',
     ],
 }
