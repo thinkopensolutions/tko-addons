c5255af093495d8944958e8ffe2efdba37e0e7c8
Yogesh Kushwaha <yogeshkushwaha4@gmail.com>
2017-05-25 18:15:45 +0000

    move modules from tko-addons-private

---
+++ tko_partner_department_function_as_m2o/__openerp__.py
@@ -0,0 +1,53 @@
+# -*- encoding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    Copyright (C) 2004-2010 Tiny SPRL (<http://tiny.be>).
+#
+#    Thinkopen Brasil
+#    Copyright (C) Thinkopen Solutions Brasil (<http://www.tkobr.com>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+{
+    'name': 'tko_partner_department_function_as_m2o',
+    'version': '0.004',
+    'category': 'Customizations',
+    'sequence': 38,
+    'complexity': 'normal',
+    'description': '''This module converts function to m2o type and adds department for partners
+''',
+    'author': 'ThinkOpen Solutions Brasil',
+    'website': 'http://www.tkobr.com',
+    'images': ['images/oerp61.jpeg',
+               ],
+    'depends': [
+        'tko_partner_department_function',
+        'tko_l10n_br_base',
+
+    ],
+    'data': [
+        'res_partner_view.xml',
+    ],
+    'init': [],
+    'demo': [],
+    'update': [],
+    'test': [],  # YAML files with tests
+    'installable': True,
+    'application': False,
+    'auto_install': False,  # If it's True, the modules will be auto-installed when all dependencies are installed
+    'certificate': '',
+}
---
+++ tko_partner_department_function_as_m2o/res_partner.py
@@ -0,0 +1,22 @@
+from openerp import fields, models, api
+
+
+class res_partner(models.Model):
+    _inherit = 'res.partner'
+
+    department_id = fields.Many2one('curriculum.department', string='Department')
+    function_id = fields.Many2one('curriculum.job.position', string='Function')
+
+    @api.onchange('function_id')
+    def onchange_function(self):
+        if self.function_id:
+            self.function = self.function_id.name
+        else:
+            self.function = False
+
+    @api.onchange('department_id')
+    def onchange_department(self):
+        if self.department_id:
+            self.departamento = self.department_id.name
+        else:
+            self.departamento = False
