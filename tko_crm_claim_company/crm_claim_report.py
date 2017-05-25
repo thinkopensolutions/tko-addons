6d6606777fd081006695b1fd0d270e34cc0d6ba9
Yogesh Kushwaha <yogeshkushwaha4@gmail.com>
2017-05-25 19:11:55 +0000

    add module tko_crm_claim_company

---
+++ tko_crm_claim_company/crm_claim_report.py
@@ -0,0 +1,69 @@
+# -*- encoding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    Copyright (C) 2004-2010 Tiny SPRL (<http://tiny.be>).
+#
+#    Thinkopen Brasil
+#    Copyright (C) Thinkopen Solutions Brasil (<http://www.tkobr.com>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+from openerp import tools
+from openerp.osv import osv
+
+
+class crm_claim_report(osv.osv):
+    """ CRM Claim Report"""
+
+    _inherit = "crm.claim.report"
+
+    def init(self, cr):
+        """ Display Number of cases And Section Name
+        @param cr: the current row, from the database cursor,
+         """
+
+        tools.drop_view_if_exists(cr, 'crm_claim_report')
+        cr.execute("""
+            create or replace view crm_claim_report as (
+                select
+                    min(c.id) as id,
+                    c.date as claim_date,
+                    c.date_closed as date_closed,
+                    c.date_deadline as date_deadline,
+                    c.user_id,
+                    c.stage_id,
+                    c.section_id,
+                    c.partner_id,
+                    c.parent_id,
+                    c.type_id,
+                    c.company_id,
+                    c.categ_id,
+                    c.name as subject,
+                    count(*) as nbr,
+                    c.priority as priority,
+                    c.type_action as type_action,
+                    c.create_date as create_date,
+                    avg(extract('epoch' from (c.date_closed-c.create_date)))/(3600*24) as  delay_close,
+                    (SELECT count(id) FROM mail_message WHERE model='crm.claim' AND res_id=c.id) AS email,
+                    extract('epoch' from (c.date_deadline - c.date_closed))/(3600*24) as  delay_expected
+                from
+                    crm_claim c
+                group by c.date,\
+                        c.user_id,c.section_id, c.stage_id,\
+                        c.categ_id,c.partner_id,c.parent_id,c.type_id,c.company_id,c.create_date,
+                        c.priority,c.type_action,c.date_deadline,c.date_closed,c.id
+            )""")
