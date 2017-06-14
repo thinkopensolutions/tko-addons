cfa2b9cefe47ab830a256e7d69dab8d0fe68554a
LucasCzepaniki <lucas.souza@tkobr.com>
2017-06-14 12:39:37 -0300

    Minor fixes

--- tko_stock_inventory_adjustments/stock.py
+++ tko_stock_inventory_adjustments/stock.py
@@ -1,26 +1,7 @@
 # -*- encoding: utf-8 -*-
-##############################################################################
-#
-#    OpenERP, Open Source Management Solution
-#    Copyright (C) 2004-2010 Tiny SPRL (<http://tiny.be>).
-#
-#    Thinkopen Brasil
-#    Copyright (C) Thinkopen Solutions Brasil (<http://www.tkobr.com>).
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
+# © 2017 TKO <http://tko.tko-br.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
 from openerp import fields, models
 
 
@@ -42,47 +23,6 @@ class stock_inventory_line(models.Model):
                                            readonly=True, string="Theoretical Quantity")
     }
 
-    def _get_inventory_lines(self, cr, uid, inventory, context=None):
-        # this super needs validation
-        parent_res = super(stock_inventory_line, self).existing(cr, uid, ids, ['location_id', 'product_id', 'package_id',
-                                                                            'product_uom_id', 'company_id',
-                                                                            'prod_lot_id', 'partner_id'], context=context)
-        location_obj = self.pool.get('stock.location')
-        product_obj = self.pool.get('product.product')
-        location_ids = location_obj.search(cr, uid, [('id', 'child_of', [inventory.location_id.id])], context=context)
-        domain = ' location_id in %s'
-        args = (tuple(location_ids),)
-        if inventory.partner_id:
-            domain += ' and owner_id = %s'
-            args += (inventory.partner_id.id,)
-        if inventory.lot_id:
-            domain += ' and lot_id = %s'
-            args += (inventory.lot_id.id,)
-        if inventory.product_id:
-            domain += ' and product_id = %s'
-            args += (inventory.product_id.id,)
-        if inventory.package_id:
-            domain += ' and package_id = %s'
-            args += (inventory.package_id.id,)
 
-        # TO DO - change query
-        cr.execute('''
-           SELECT product_id, sum(qty) as product_qty, location_id, lot_id as prod_lot_id, package_id, owner_id as partner_id
-           FROM stock_quant WHERE''' + domain + '''
-           GROUP BY product_id, location_id, lot_id, package_id, partner_id
-        ''', args)
-        vals = []
-        for product_line in cr.dictfetchall():
-            #replace the None the dictionary by False, because falsy values are tested later on
-            for key, value in product_line.items():
-                if not value:
-                    product_line[key] = False
-            product_line['inventory_id'] = inventory.id
-            product_line['theoretical_qty'] = product_line['product_qty']
-            if product_line['product_id']:
-                product = product_obj.browse(cr, uid, product_line['product_id'], context=context)
-                product_line['product_uom_id'] = product.uom_id.id
-            vals.append(product_line)
-        return vals
 
 # add the options for line creation?
