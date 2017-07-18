12b235d35f5e65c4ce1fe83c907902ba72f8a53e
Yogesh Kushwaha <yogeshkushwaha4@gmail.com>
2017-07-18 23:32:58 +0000

    FIX inheritance

--- tko_coexiste_br_account/models/analytic_account.py
+++ tko_coexiste_br_account/models/analytic_account.py
@@ -2,7 +2,7 @@
 from odoo import models, fields, api, _
 
 class AccountAnalyticLine(models.Model):
-    _name = 'account.analytic.line'
+    _inherit = 'account.analytic.line'
 
     partner_id = fields.Many2one('res.partner', related='move_id.partner_id', string='Partner', store=True)
     company_id = fields.Many2one(related='move_id.company_id', string='Company', store=True, readonly=False)
