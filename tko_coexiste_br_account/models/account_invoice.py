3cc24e98dae2ff8a0fa00d5aa558bbd36e34fecb
Devishree Brahmbhatt <brahm.devi@gmail.com>
2017-08-15 01:17:54 +0530

    [IMP]tko_coexiste_br_account: Set Move line date maturity as same as Payable/Receivable Account Line .

--- tko_coexiste_br_account/models/account_invoice.py
+++ tko_coexiste_br_account/models/account_invoice.py
@@ -35,6 +35,24 @@ class AccountExpenseType(models.Model):
                                     required=True, default='b', string='InvoiceType')
 
 
+class AccountMove(models.Model):
+    _inherit = 'account.move'
+
+    @api.model
+    def create(self, vals):
+        res=super(AccountMove, self).create(vals) 
+        move_line_ids = self.env['account.move.line'].search([('move_id','=',res.id)])
+        partner = res.partner_id
+        account_ids = []
+        account_ids.append(partner.property_account_receivable_id.id)
+        account_ids.append(partner.property_account_payable_id.id)
+        partner_line_id = self.env['account.move.line'].search([('move_id','=',res.id),('account_id','in',account_ids)])
+        date_maturity = partner_line_id.date_maturity
+        for line in move_line_ids:
+            line.update({'date_maturity': date_maturity})
+        return res
+
+
 class AccountPayment(models.Model):
     _inherit = 'account.payment'
 
