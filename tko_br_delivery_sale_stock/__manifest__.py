3950328cc21da8875de9c6bb9deb7b1b5a9e638c
Yogesh Kushwaha <yogeshkushwaha4@gmail.com>
2017-09-28 18:22:36 -0300

    add module tko_br_delivery_sale_stock

---
+++ tko_br_delivery_sale_stock/__manifest__.py
@@ -0,0 +1,29 @@
+# -*- encoding: utf-8 -*-
+
+{
+    'name': 'tko_br_delivery_sale_stock',
+    'version': '10.0',
+    'category': 'customization',
+    'sequence': 150,
+    'complexity': 'normal',
+    'description': '''  Customize claim form  ''',
+    'author': 'ThinkOpen Solutions Brasil',
+    'website': 'http://www.tkobr.com',
+    'images': [
+        'images/oerp61.jpeg',
+    ],
+    'depends': [
+       'br_sale_stock',
+        'delivery',
+    ],
+    'data': [
+    ],
+    'init': [],
+    'demo': [],
+    'update': [],
+    'test': [],  # YAML files with tests
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+    'certificate': '',
+}
