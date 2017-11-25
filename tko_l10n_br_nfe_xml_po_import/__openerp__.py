def82662f13e86f0e45e768dbbc4974d95246e99
Yogesh Kushwaha <yogeshkushwaha4@gmail.com>
2017-11-25 00:02:19 -0200

    create purchase Order from XML import

---
+++ tko_l10n_br_nfe_xml_po_import/__openerp__.py
@@ -0,0 +1,59 @@
+# -*- encoding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    Copyright (C) 2004-2010 Tiny SPRL (<http://tiny.be>).
+#
+#    Thinkopen Brasil
+#    Copyright (C) Thinkopen Solutions Brasil (<http://www.tkobr.com>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+{
+    'name': 'tko_l10n_br_account_supplier_xml_import',
+    'version': '0.041',
+    'category': 'Customizations',
+    'sequence': 120,
+    'complexity': 'normal',
+    'description': '''This module creates supplier invoices from NFe XML import''',
+    'author': 'ThinkOpen Solutions Brasil',
+    'website': 'http://www.tkobr.com',
+    'images': ['images/oerp61.jpeg',
+               ],
+    'depends': [
+        'account',
+        'document',
+        'stock',  # to set product type product
+        'tko_l10n_br_base',
+        'l10n_br_account',
+        'l10n_br_account_product',
+    ],
+    'data': [
+        # 'data/account_tax_code_template.xml',
+        # 'data/update_tax_codes_from_templates.xml',
+        # 'account_invoice_view.xml',
+        'nfe_attachment_view.xml',
+        # 'account_tax_code_view.xml',
+    ],
+    'init': [],
+    'demo': [],
+    'update': [],
+    'test': [],  # YAML files with tests
+    'installable': True,
+    'application': False,
+    'auto_install': False,  # If it's True, the modules will be auto-installed when all dependencies are installed
+    'certificate': '',
+}
