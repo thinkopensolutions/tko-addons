fbd71be76efbfdb4748a99d25977eb46c286ab02
yk2kus <yogeshkushwaha4@gmail.com>
2018-02-19 17:22:47 -0300

    add field Expense Type on reconciliation widget

--- tko_coexiste_br_account/static/src/js/account_reconciliation_widgets.js
+++ tko_coexiste_br_account/static/src/js/account_reconciliation_widgets.js
@@ -1,22 +1,25 @@
 odoo.define('tko_coexiste_br_account.reconciliationa', function (require) {
 "use strict";
-
+    var core = require('web.core');
+    var _t = core._t;
     var reconciliation = require('account.reconciliation');
+    var FieldMany2One = core.form_widget_registry.get('many2one');
 
     reconciliation.abstractReconciliation.include({
         init: function(parent, context) {
             this._super(parent, context);
-            this.create_form_fields['analytic_account_id']= {
-                id: "analytic_account_id",
-                index: 20,
-                corresponding_property: "analytic_account_id",
-                label: _t("Analytic Acc."),
+            var self = this;
+            this.create_form_fields['expense_type_id']= {
+                id: "expense_type_id",
+                index: 25,
+                corresponding_property: "expense_type_id",
+                label: _t("Expense Type"),
                 required: false,
                 group:"analytic.group_analytic_accounting",
                 constructor: FieldMany2One,
                 field_properties: {
-                    relation: "account.analytic.account",
-                    string: _t("Analytic Acc."),
+                    relation: "account.expense.type",
+                    string: _t("Expense Type"),
                     type: "many2one",
                 },
             }
