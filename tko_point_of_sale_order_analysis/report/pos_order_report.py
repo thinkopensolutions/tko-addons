9d3091adf9f4630470c43d9ef596b083cc7eb02e
lucasczpnk <lucas.souza@tkobr.com>
2018-02-27 19:28:14 -0300

    Fixed query/field issues!

--- tko_point_of_sale_order_analysis/report/pos_order_report.py
+++ tko_point_of_sale_order_analysis/report/pos_order_report.py
@@ -40,75 +40,69 @@ class report_pos_order_tko(osv.osv):
         'location_id': fields.many2one('stock.location', 'Location', readonly=True),
         'company_id': fields.many2one('res.company', 'Company', readonly=True),
         'nbr': fields.integer('# of Lines', readonly=True),  # TDE FIXME master: rename into nbr_lines
-        'journal_id': fields.many2one('account.journal', 'Journal'),
         'order_id': fields.many2one('pos.order', 'Order'),
         'total_discount': fields.float('Total Discount', readonly=True),
         'total_liquid': fields.float('Total Liquid', readonly=True),
         'average_price': fields.float('Average Price', readonly=True, group_operator="avg"),
-        'order_type': fields.selection([('v', 'Venda'), ('t', 'Troca'), ('c', 'Cancelamento')],string=u'Order Type', readonly=True),
+        'order_type': fields.selection([('v', 'Venda'), ('t', 'Troca'), ('c', 'Cancelamento')], string=u'Order Type', readonly=True),
+        'discount_card_name': fields.char(u'Discount Card'),
+        'discount_card_type': fields.selection([('p', 'Percentage'), ('fi', 'Fixed')], string=u'Discount Type'),
         'session_id': fields.many2one('pos.session', 'Session',select=1,domain="[('state', '=', 'opened')]",states={'draft' : [('readonly', False)]},readonly=True),
-        'account_move': fields.many2one('account.move', 'Journal Entry', readonly=True, copy=False),
-        'discount_card_id': fields.many2one('pos.discount.cards', 'Discount Cards'),
-        'name': fields.char('Discount Card'),
-        'type': fields.selection([('p', 'Percentage'), ('fi', 'Fixed')], string='Type'),
-        'value': fields.char('Card Value'),
         'discount_on_order': fields.float('Discount on Order'),
-        'move_id': fields.many2one('account.move', 'Account Move', ondelete="cascade", help="The move of this entry line.", select=2, required=True, auto_join=True),
-        'bank_statement_id': fields.many2one('account.bank.statement', 'Statement', select=True, required=True, ondelete='restrict'),
-        'ref': fields.char('Reference'),
+        'statement_id': fields.many2one('account.bank.statement', 'Statement', select=True, required=True, ondelete='restrict'),
+        'journal_id': fields.related('statement_id', 'journal_id', type='many2one', relation='account.journal', string=u'Journal', store=True, readonly=True),
+        'name': fields.related('account_move', 'id', type='char', relation='account.move', string=u'Move Number', readonly=True),
+        'price_total': fields.float('Total Price', readonly=True),
+        'total_discount': fields.float('Total Discount', readonly=True),
+        'product_id': fields.many2one('product.product', 'Product', readonly=True),
+        'product_qty': fields.integer('Product Quantity', readonly=True),
+        'product_categ_id': fields.many2one('product.category', 'Product Category', readonly=True),
 
     }
     _order = 'date desc'
 
     def init(self, cr):
-        tools.drop_view_if_exists(cr, 'report_pos_order') #rename to report_pos_order and remove from pos_order.py
+        tools.drop_view_if_exists(cr, 'report_pos_order_tko')
         cr.execute("""
-            create or replace view report_pos_order as (
+            create or replace view report_pos_order_tko as (
                 select
-                    min(l.id) as id,
+                    min(o.id) as id,
                     count(*) as nbr,
-                    s.date_order as date,
                     sum(l.qty * u.factor) as product_qty,
                     sum(l.qty * l.price_unit) as price_total,
-                    sum((l.qty * l.price_unit) * (l.discount / 100) + coalesce((s.discount_on_order / s.no_lines ),0.0)) as total_discount,
-                    sum(l.line_subototal) - coalesce(sum(s.discount_on_order / s.no_lines ),0.0) as total_liquid,
-                    (sum(l.qty*l.price_unit)/sum(l.qty * u.factor))::decimal as average_price,
-                    sum(cast(to_char(date_trunc('day',s.date_order) - date_trunc('day',s.create_date),'DD') as int)) as delay_validation,
-                    s.partner_id as partner_id,
-                    s.state as state,
-                    s.user_id as user_id,
-                    s.location_id as location_id,
-                    s.company_id as company_id,
-                    s.sale_journal as sale_journal,
+                    o.location_id as location_id,
+                    o.order_type as order_type,
+                    o.discount_card_name as discount_card_name,
+                    o.discount_card_type as discount_card_type,
+                    o.session_id as session_id,
+                    o.discount_on_order as discount_on_order,
+                    b.statement_id as statement_id,
+                    b.journal_id as journal_id,
+                    m.name as account_move,
                     l.product_id as product_id,
-                    pt.categ_id as product_categ_id,
-                    l.order_id as order_id,
-                    s.order_type as order_type,
-                    al.statement_id as bank_statement_id,
-                    al.ref as bank_statement_reference,
-                    s.session_id as session_id,
-                    m.journal_id as journal_id,
-                    s.account_move as account_move,
-                    d.name as discount_card,
-                    d.type as discount_type,
-                    d.value as discount_value,
-                    s.discount_on_order as discount_on_order,
-                    m.move_id as account_move_line_id
+                    t.categ_id as product_categ_id,
+                    o.partner_id as partner_id,
+                    o.date_order as date,
+                    o.user_id as user_id,
+                    o.company_id as company_id,
+                    o.state as state,
+                    (AVG((l.qty * l.price_unit) - ((l.qty * l.price_unit) * (l.discount / 100) + coalesce(o.discount_on_order / o.no_lines ))) ) as average_price,
+                    sum((l.qty * l.price_unit) * (l.discount / 100) + (o.discount_on_order / o.no_lines )) as total_discount,
+                    sum((l.qty * l.price_unit) - ((l.qty * l.price_unit) * (l.discount / 100) + coalesce(o.discount_on_order / o.no_lines ))) as total_liquid,
+                    l.order_id as order_id
                     
-                from pos_order_line as l
-                    left join pos_order s on (s.id=l.order_id)
-                    left join product_product p on (p.id=l.product_id)
-                    left join product_template pt on (pt.id=p.product_tmpl_id)
-                    left join product_uom u on (u.id=pt.uom_id)
-                    left join pos_discount_cards d on (d.id=s.discount_card_id)
-                    left join account_bank_statement_line al on (al.pos_statement_id=s.id)
-                    left join account_move_line m on (m.statement_id=al.statement_id)
-                    
+                from pos_order as o
+                     left join pos_order_line l on (o.id=l.order_id)
+                     left join account_bank_statement_line b on (b.pos_statement_id=o.id)
+                     left join account_move m on (m.id=o.account_move)
+                     left join product_product p on (p.id=l.product_id)
+                     left join product_template t on (t.id=p.product_tmpl_id)
+                     left join product_uom u on (u.id=t.uom_id)
+                
                 group by
-                    s.date_order, s.partner_id,s.state, pt.categ_id,
-                    s.user_id,s.location_id,s.company_id,s.sale_journal,l.product_id,s.create_date,l.order_id,s.no_lines,s.discount_on_order,l.line_subototal,
-                    s.order_type,al.statement_id,s.session_id,m.journal_id,s.account_move,d.name,d.type,
-                    m.move_id,al.ref,d.value
+                   o.user_id,o.partner_id,o.company_id,o.location_id,o.order_type,o.discount_card_name,discount_card_type,
+                   o.session_id,o.discount_on_order,b.statement_id,b.journal_id,m.name,l.product_id,t.categ_id,
+                   o.state,o.date_order,l.order_id
                 having
                     sum(l.qty * u.factor) != 0)""")
 
