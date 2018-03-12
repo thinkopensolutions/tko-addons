7a6e21892ed4b65235f9cf8e7776e634d6eb8844
yk2kus <yogeshkushwaha4@gmail.com>
2018-03-12 18:34:44 -0300

    do not create any invoice without invoice line

--- tko_contract_invoicing_product_effective_date/models/account_analytic_account.py
+++ tko_contract_invoicing_product_effective_date/models/account_analytic_account.py
@@ -2,26 +2,37 @@
 
 from odoo import models, fields, api, _
 from odoo.exceptions import Warning
+import logging
+_logger = logging.getLogger(__name__)
 
 class AccountAnalyticAccont(models.Model):
     _inherit = 'account.analytic.account'
 
     end_date = fields.Date(u'End Date')
 
+    def _validate_invoice_creating(self):
+        for line in self.recurring_invoice_line_ids:
+            if line.start_date <= self.recurring_next_date and line.end_date >=self.recurring_next_date:
+                return True
+        return False
+
     # Add invoce lines only Contract's Date of Next Invoice
     # falls in the range of start and end date of line
     @api.multi
     def _create_invoice(self):
-        self.ensure_one()
-        invoice_vals = self._prepare_invoice()
-        invoice = self.env['account.invoice'].create(invoice_vals)
-        for line in self.recurring_invoice_line_ids:
-            if line.start_date >= self.recurring_next_date and self.recurring_next_date <= line.end_date:
-                invoice_line_vals = self._prepare_invoice_line(line, invoice.id)
-                self.env['account.invoice.line'].create(invoice_line_vals)
-        invoice.compute_taxes()
-        return invoice
-
+        if self._validate_invoice_creating():
+            self.ensure_one()
+            invoice_vals = self._prepare_invoice()
+            invoice = self.env['account.invoice'].create(invoice_vals)
+            for line in self.recurring_invoice_line_ids:
+                if line.start_date <= self.recurring_next_date and line.end_date >= self.recurring_next_date:
+                    invoice_line_vals = self._prepare_invoice_line(line, invoice.id)
+                    self.env['account.invoice.line'].create(invoice_line_vals)
+            invoice.compute_taxes()
+            return invoice
+        else:
+            _logger.warn(u"No Invoice to be created for date %s" %self.recurring_next_date)
+            return True
 
 
 
