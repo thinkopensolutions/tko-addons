b7aa0419c40415f2de0495a156d6f70faf2585dd
yk2kus <yogeshkushwaha4@gmail.com>
2018-03-12 16:11:11 -0300

    add new field for contract sequence

--- tko_contract/models/contract.py
+++ tko_contract/models/contract.py
@@ -6,9 +6,11 @@ from odoo import models, fields, api, _
 class AccountAnalyticAccount(models.Model):
     _inherit = 'account.analytic.account'
 
+    contract_sequence = fields.Char('Sequence', readonly=True)
+
     @api.model
     def create(self, vals):
         sequence = self.env['ir.sequence'].next_by_code(
             'account.analytic.account') or 'New'
-        vals.update({'code': sequence})
+        vals.update({'contract_sequence': sequence})
         return super(AccountAnalyticAccount, self).create(vals)
