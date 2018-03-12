58d94d68f39e02a1d42e5a5b39b85751f8591161
yk2kus <yogeshkushwaha4@gmail.com>
2018-03-12 18:18:10 -0300

    create invoice only for valid lines

--- tko_contract_invoicing_product_effective_date/models/account_analytic_account.py
+++ tko_contract_invoicing_product_effective_date/models/account_analytic_account.py
@@ -8,52 +8,22 @@ class AccountAnalyticAccont(models.Model):
 
     end_date = fields.Date(u'End Date')
 
-    api.multi
-
+    # Add invoce lines only Contract's Date of Next Invoice
+    # falls in the range of start and end date of line
+    @api.multi
     def _create_invoice(self):
-        dates = self.recurring_invoice_line_ids.mapped('start_date')
-        unique_invoices = {}
-        for line in self.recurring_invoice_line_ids:
-            if line.start_date in unique_invoices.keys():
-                unique_invoices[line.start_date].append(line)
-            else:
-                unique_invoices[line.start_date] = [line]
-
-
         self.ensure_one()
-        # create separate invoice for each separate group of date
-        for key,vals in unique_invoices.iteritems():
-            invoice_vals = self._prepare_invoice()
-            invoice = self.env['account.invoice'].create(invoice_vals)
-            invoice.write({'date_invoice': key})
-            # create invoice lines
-            for line in vals:
+        invoice_vals = self._prepare_invoice()
+        invoice = self.env['account.invoice'].create(invoice_vals)
+        for line in self.recurring_invoice_line_ids:
+            if line.start_date >= self.recurring_next_date and self.recurring_next_date <= line.end_date:
                 invoice_line_vals = self._prepare_invoice_line(line, invoice.id)
                 self.env['account.invoice.line'].create(invoice_line_vals)
-
-            invoice.compute_taxes()
+        invoice.compute_taxes()
         return invoice
 
-    @api.multi
-    def recurring_create_invoice(self):
-        for contract in self:
-            old_date = fields.Date.from_string(
-                contract.recurring_next_date or fields.Date.today())
-            new_date = old_date + self.get_relative_delta(
-                contract.recurring_rule_type, contract.recurring_interval)
-            ctx = self.env.context.copy()
-            ctx.update({
-                'old_date': old_date,
-                'next_date': new_date,
-                # Force company for correct evaluate domain access rules
-                'force_company': contract.company_id.id,
-            })
-            # Re-read contract with correct company
-            contract.with_context(ctx)._create_invoice()
-            contract.write({
-                'recurring_next_date': new_date.strftime('%Y-%m-%d')
-            })
-        return True
+
+
 
 
 class AccountAnalyticInvoiceLine(models.Model):
