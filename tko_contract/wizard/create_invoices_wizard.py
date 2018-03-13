df6d07fe6023dd6ac7cd5501889b8c8fc6188c01
yk2kus <yogeshkushwaha4@gmail.com>
2018-03-13 11:59:21 -0300

    create mass invocies

---
+++ tko_contract/wizard/create_invoices_wizard.py
@@ -0,0 +1,13 @@
+# -*- encoding: utf-8 -*-
+
+from odoo import models, fields, api, _
+
+
+class CreateInvoicesWizard(models.Model):
+    _name = 'create.invoices.wizard'
+
+    # Create Mass Invoices
+    @api.multi
+    def create_invoices(self):
+        active_ids = self._context.get('active_ids', [])
+        return self.env['account.analytic.account'].browse(active_ids).recurring_create_invoice()
